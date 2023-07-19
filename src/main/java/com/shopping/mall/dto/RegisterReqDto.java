ddeb851a645d0665141fe6e2ceea5b3626822503
DESKTOP-4F871ER\이호승 <lhs199653@gmail.com>
2023-07-19 17:18:18 +0900

    로그인 수정

--- src/main/java/com/shopping/mall/dto/RegisterReqDto.java
+++ src/main/java/com/shopping/mall/dto/RegisterReqDto.java
@@ -13,14 +13,9 @@ import javax.validation.constraints.Size;
 @Data
 public class RegisterReqDto {
     @NotBlank(message = "이름은 비워둘 수 없습니다", groups = ValidationGroups.NotBlankGroup.class)
-    @Size(min = 1, max = 3, message = "이름은 3글자까지만 입력 가능합니다", groups = ValidationGroups.SizeGroup.class)
+    @Size(min = 1, max = 3, message = "이름은 4글자까지만 입력 가능합니다", groups = ValidationGroups.SizeGroup.class)
     @Pattern(regexp = "^[가-힇]*$", message = "한글만 입력 가능합니다", groups = ValidationGroups.PatternCheckGroup.class)
-    private String familyName;
-
-    @NotBlank(message = "성은 비워둘 수 없습니다", groups = ValidationGroups.NotBlankGroup.class)
-    @Size(min = 1, max = 2, message = "성은 2글자까지만 입력 가능합니다", groups = ValidationGroups.SizeGroup.class)
-    @Pattern(regexp = "^[가-힇]*$", message = "한글만 입력 가능합니다", groups = ValidationGroups.PatternCheckGroup.class)
-    private String firstName;
+    private String name;
 
     @NotBlank(message = "이메일은 비워둘 수 없습니다", groups = ValidationGroups.NotBlankGroup.class)
     @Email
@@ -34,7 +29,7 @@ public class RegisterReqDto {
     public User toEntity() {
         return User.builder()
                 .password(new BCryptPasswordEncoder().encode(password)) // 비밀번호 암호화
-                .name(firstName + familyName)
+                .name(name)
                 .email(email)
                 .role_id(1)
                 .build();
